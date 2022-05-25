4a1e764d354121f2ad1d6f87f007a3c8e7e7e479
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-25 06:44:02 +0300

    dz15
    dz15

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "pass123";
+            string secretMessage = "Это секретное сообщение";
+            int inputCounter = 3;
+            string userInputPassword;
+
+            Console.Write("Введите пароль: ");
+            userInputPassword = Console.ReadLine();
+
+            if (password == userInputPassword)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(secretMessage);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                while (inputCounter > 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Вы ввели неверный пароль. Оставшееся количество попыток: " + (inputCounter - 1)  + " Введите пароль: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    userInputPassword = Console.ReadLine();
+
+                    if (password == userInputPassword)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(secretMessage);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    }
+                    inputCounter--;
+
+                    if(inputCounter == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Вы исчерпали все попытки. Прощайте.");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    }
+                }
+                
+            }
+
+        }
+    }
+}
