5c65c741ca2daa3ac463325cfc80ff04c1bf2d0f
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-30 23:23:57 +0300

    dz15
    dz15

--- Program.cs
+++ Program.cs
@@ -13,40 +13,29 @@ namespace vn_mode_csharp_dz15
 
             Console.Write("Введите пароль: ");
             userInputPassword = Console.ReadLine();
-
-            if (password == userInputPassword)
+            for ( ; inputCounter > 0; )
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(secretMessage);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else
-            {
-                while (inputCounter > 1)
+                if (password == userInputPassword)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(secretMessage);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                }
+                else if(inputCounter > 1)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("Вы ввели неверный пароль. Оставшееся количество попыток: " + (inputCounter - 1)  + " Введите пароль: ");
+                    Console.WriteLine($"Вы ввели неверный пароль. Оставшееся количество попыток: {--inputCounter}. Введите пароль:");
                     Console.ForegroundColor = ConsoleColor.White;
                     userInputPassword = Console.ReadLine();
-
-                    if (password == userInputPassword)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(secretMessage);
-                        Console.ForegroundColor = ConsoleColor.White;
-                        break;
-                    }
-                    inputCounter--;
-
-                    if(inputCounter == 1)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Вы исчерпали все попытки. Прощайте.");
-                        Console.ForegroundColor = ConsoleColor.White;
-
-                    }
                 }
-                
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Вы исчерпали все попытки. Прощайте.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                }
             }
 
         }
